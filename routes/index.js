d69156df2ee51ea0c33dc828af5093e073074e94
Jennesy <jennesy.dai@gmail.com>
2021-06-18 14:36:31 +0800

    feat: add restaurant index page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
+const restController = require('../controllers/restController')
 module.exports = (app) => {
-
+  app.get('/restaurants', restController.getRestaurants)
   app.get('/', (req, res) => {
     res.send('Hello World!')
   })
