11bcdf2e0deb62276609f1feea8ecc4b9f4e1c6c
Jennesy <jennesy.dai@gmail.com>
2021-06-20 23:10:06 +0800

    feat: add admin user index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
+const User = db.User
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
@@ -115,6 +116,12 @@ let adminController = {
           .then((restaurant) => {
             return res.redirect('/admin/restaurants')
           })
+      })
+  },
+  getUsers: (req, res) => {
+    User.findAll({ raw: true })
+      .then(users => {
+        return res.render('admin/users', { users })
       })
   }
 }
